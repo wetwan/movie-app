8000bbdc7a6e52f4439e1d4357c8d6d706297d87
ridwanullah <wetwan080@gmail.com>
2025-03-26 15:33:51 +0100

    search componets

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -8,10 +8,10 @@ const TabIcon = ({ focused, icon, title }: any) => {
     return (
       <ImageBackground
         source={images.highlight}
-        className="flex flex-row w-full flex-1 min-w-[112px] min-h-16 mt-4 justify-center items-center rounded-full overflow-hidden"
+        className="flex  flex-row w-full flex-1 min-w-[112px] min-h-14 mt-4 justify-center items-center rounded-full overflow-hidden"
       >
         <Image source={icon} tintColor="#151312" className="size-5" />
-        <Text className="text-secondary text-base font-semibold ml-2 capitalize">
+        <Text className="text-secondary text-base font-semibold ml-2  capitalize">
           {title}
         </Text>
       </ImageBackground>
---
+++ components/Searchbar.tsx
@@ -0,0 +1,32 @@
+import { View, Text, Image, TextInput } from "react-native";
+import React, { useState } from "react";
+import { icons } from "@/constants/icons";
+
+type Props = {
+  placeholder: string;
+  onPress?: () => void;
+};
+
+const Searchbar = ({ placeholder, onPress }: Props) => {
+  const [] = useState();
+  return (
+    <View className="flex-row items-center bg-dark-200 rounded-full px-5 py-4">
+      <Image
+        source={icons.search}
+        className="size-5"
+        resizeMode="contain"
+        tintColor="#ab8bff"
+      />
+      <TextInput
+        onPress={onPress}
+        placeholder={placeholder}
+        value=""
+        onChangeText={() => {}}
+        placeholderTextColor="#ab8bff"
+        className="flex-1 ml-2 text-white"
+      />
+    </View>
+  );
+};
+
+export default Searchbar;
