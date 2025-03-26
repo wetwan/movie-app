33d7724f745e323c8ed29b384c2257d9070bf741
ridwanullah <wetwan080@gmail.com>
2025-03-26 15:07:17 +0100

    home layout and nav design

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   // NOTE: Update this to include the paths to all of your component files.
-  content: ["./app/**/*.{js,jsx,ts,tsx}"],
+  content: ["./app/**/*.{js,jsx,ts,tsx}","./compon/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
     extend: {
@@ -17,6 +17,7 @@ module.exports = {
         dark: {
           100: "#221F3D",
           200: "#0F0D23",
+   
         },
       },
     },
